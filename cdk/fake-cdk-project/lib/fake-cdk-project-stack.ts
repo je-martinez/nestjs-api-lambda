a435a609228c7ec88a9d8d30c761a368561147d4
Jose E. Martinez <josemartinezchevez@gmail.com>
2025-02-17 15:15:29 -0600

    feat: wip api gateway

--- cdk/fake-cdk-project/lib/fake-cdk-project-stack.ts
+++ cdk/fake-cdk-project/lib/fake-cdk-project-stack.ts
@@ -1,19 +1,58 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as signer from 'aws-cdk-lib/aws-signer';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path';
+import * as restApi from 'aws-cdk-lib/aws-apigateway';
 
 export class FakeCdkProjectStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'FakeCdkProjectQueue', {
-      visibilityTimeout: Duration.seconds(300),
+    const signingProfile = new signer.SigningProfile(this, 'SigningProfile', {
+      platform: signer.Platform.AWS_LAMBDA_SHA384_ECDSA,
     });
 
-    const topic = new sns.Topic(this, 'FakeCdkProjectTopic');
+    const codeSigningConfig = new lambda.CodeSigningConfig(
+      this,
+      'CodeSigningConfig',
+      {
+        signingProfiles: [signingProfile],
+      },
+    );
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const apiNestHandlerFunction = new lambda.Function(this, 'NestJsLambda', {
+      codeSigningConfig,
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'lambda.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../../dist')),
+    });
+
+    const api = new restApi.RestApi(this, 'NestJsLambdaApi', {
+      deploy: true,
+      defaultMethodOptions: {
+        apiKeyRequired: true,
+      },
+    });
+
+    api.root.addProxy({
+      defaultIntegration: new restApi.LambdaIntegration(
+        apiNestHandlerFunction,
+        {
+          proxy: true,
+        },
+      ),
+    });
+    const apiKey = api.addApiKey('ApiKey');
+    const usagePlan = api.addUsagePlan('UsagePlan', {
+      name: 'UsagePlan',
+      apiStages: [
+        {
+          api,
+          stage: api.deploymentStage,
+        },
+      ],
+    });
+    usagePlan.addApiKey(apiKey);
   }
 }
