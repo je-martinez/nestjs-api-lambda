8e85e3721a517cd5eb3988929829de487ff407bc
Jose E. Martinez <josemartinezchevez@gmail.com>
2025-02-18 12:06:58 -0600

    refactor: use one webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,27 @@
-const nodeExternals = require('webpack-node-externals');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
+const lazyImports = [
+  '@nestjs/microservices/microservices-module',
+  '@nestjs/websockets/socket-module',
+];
 module.exports = function (options, webpack) {
-  const lazyImports = [
-    '@nestjs/microservices/microservices-module',
-    '@nestjs/websockets/socket-module',
-  ];
-
   return {
     ...options,
     entry: ['./src/lambda.ts'],
-    externals: [nodeExternals()],
-    mode: 'production',
+    externals: [
+      ({ request }, callback) => {
+        // Exclude non-essential modules
+        if (lazyImports.includes(request)) {
+          return callback(null, `commonjs ${request}`);
+        }
+        // Exclude Cursor & IDE-specific extensions
+        if (request.includes('.cursor/extensions/')) {
+          return callback(null, `commonjs ${request}`);
+        }
+        callback();
+      },
+    ],
     output: {
       ...options.output,
       libraryTarget: 'commonjs2',
-      filename: 'lambda.js',
-    },
-    resolve: {
-      extensions: ['.ts', '.js'],
-      plugins: [new TsconfigPathsPlugin()],
     },
     plugins: [
       ...options.plugins,
@@ -30,14 +32,5 @@ module.exports = function (options, webpack) {
         },
       }),
     ],
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
-      ],
-    },
   };
 };
